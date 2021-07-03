c955e796fd654fb4f98a858b6363b71f33daf954
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-07-04 01:28:28 +0500

    implemented StarRating component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { FaStar } from "react-icons/fa";
+
+const createArray = (length) => [...Array(length)]
+
+const Star = ({ selected = false, onSelect }) => <FaStar onClick={onSelect} color={selected ? 'red' : 'gray'} />
+
+const StarRating = ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <div>
+      {createArray(totalStars).map((n, i) => (<Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />))}
+      <p>({selectedStars} of {totalStars})</p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StarRating totalStars={10} />
     </div>
   );
 }
