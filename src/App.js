7a6cb57ef51e58efb45b3519617288ce45f304e4
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-07-04 14:38:45 +0500

    adding-counter made using useReducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useReducer((number, newNumber) => number + newNumber, 0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 onClick={() => setNumber(1)}>{number}</h1>
   );
 }
 
