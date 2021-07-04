b9f2f7ac1c1fef3f8adc10ca138372ee0b638a55
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2021-07-04 15:02:29 +0500

    dispatching actions with useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { useReducer } from 'react';
 import './App.css';
 
+const initialState = {
+  message: 'hi'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'yell':
+      return { message: state.message + ' => HEY!' } // state.message returns previous message
+    case 'whisper':
+      return { message: state.message + ' => Excuse me' }
+    default:
+      return { message: 'default' }
+  }
+}
+
 function App() {
-  const [checked, toggle] = useReducer((checked) => !checked, false)
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
-      <input type='checkbox' value={checked} onChange={toggle} />
-      {checked ? 'checked' : 'not checked'}
+      <p>Message: {state.message}</p>
+      <button onClick={() => dispatch({ type: 'yell' })}>YELL</button>
+      <button onClick={() => dispatch({ type: 'whisper' })}>WHISPER</button>
     </>);
 }
 
